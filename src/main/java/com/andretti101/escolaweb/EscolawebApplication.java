df6cc6fb5218e178df6cf7156c2167bfe105d0c3
andretti101 <enzoandretti3@gmail.com>
2026-05-21 21:08:44 -0300

    first commit

---
+++ src/main/java/com/andretti101/escolaweb/EscolawebApplication.java
@@ -0,0 +1,13 @@
+package com.andretti101.escolaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscolawebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscolawebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andretti101/escolaweb/EscolawebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andretti101.escolaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolawebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
